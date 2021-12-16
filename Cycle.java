32f1fd907cf6c97d1213e127e268d2d0b96f64f3
Evgenii Ptitsyn <eptitsyn@gmail.com>
2021-12-16 17:56:47 +0300

    hw3 last part

--- Cycle.java
+++ Cycle.java
@@ -6,12 +6,14 @@ public class Cycle {
 		for (int i = -6; i <= 6; i += 2) {
 			System.out.println(i);
 		}
-		int sum;
+		int sum = 0;
 		int i = 10;
 		do {
 			if(!(i % 2 == 0)) {
 				sum += i;
 			}
+			i++;
 		} while (i <= 20);
+		System.out.println("sum odd of [10, 20] = " + i);
 	}
 }
