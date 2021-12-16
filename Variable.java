99a3fcef8d5be3c7e5350cf43e370372d5361670
Evgenii Ptitsyn <eptitsyn@gmail.com>
2021-12-16 16:41:06 +0300

    init

---
+++ Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+	public static void main(String[] args) {
+		byte usbPortsCount = 3;
+		short screenSize = 16;
+		int cpuFrequeency = 2000;
+		long hddCapacity = 1000;
+		float bodyThickness = (float) 23.6;
+		double bezelThikness = 7.3;
+		char modelLetter = 'A';
+		boolean isLaptop = true;
+	}
+}
