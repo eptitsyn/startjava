fac6710c52374de6173f92fc6bbe0c82e0c2f0d7
Evgenii Ptitsyn <eptitsyn@gmail.com>
2021-12-18 14:28:23 +0300

    wolf getters/setters

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,10 +1,50 @@
 public class Wolf {
 
-    String name;
-    char sex;
-    float weight;
-    int age;
-    String color;
+    private String name;
+    private char sex;
+    private float weight;
+    private int age;
+    private String color;
+
+    public String getName() {
+        return name;
+    };
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getSex() {
+        return sex;
+    };
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public float getWeight() {
+        return weight;
+    };
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    };
+    public void setAge(int age) {
+        if (age <= 8) {
+            this.age = age;
+        } else {
+            System.out.println("Некорректный возраст");
+        }
+    }
+
+    public String getColor() {
+        return color;
+    };
+    public void setColor(String color) {
+        this.color = color;
+    }
+
 
     public void walk() {
         System.out.println("Walking.");
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,18 +2,18 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolfLeader = new Wolf();
-        wolfLeader.name = "Akella";
-        wolfLeader.sex = 'M';
-        wolfLeader.weight = 47.5f;
-        wolfLeader.age = 12;
-        wolfLeader.color = "Pale";
+        wolfLeader.setName("Akella");
+        wolfLeader.setSex('M');
+        wolfLeader.setWeight(47.5f);
+        wolfLeader.setAge(7);
+        wolfLeader.setColor("Pale");
 
-        System.out.println("Wolf: " + wolfLeader.name);
-        System.out.println("sex: " + wolfLeader.sex);
-        System.out.println("weight: " + wolfLeader.weight);
-        System.out.println("age: " + wolfLeader.age);
-        System.out.println("weight: " + wolfLeader.weight);
-        System.out.println("color: " + wolfLeader.color);
+        System.out.println("Wolf: " + wolfLeader.getName());
+        System.out.println("sex: " + wolfLeader.getSex());
+        System.out.println("weight: " + wolfLeader.getWeight());
+        System.out.println("age: " + wolfLeader.getAge());
+        System.out.println("weight: " + wolfLeader.getWeight());
+        System.out.println("color: " + wolfLeader.getColor());
 
         wolfLeader.walk();
         wolfLeader.sit();
