b34f05615a40b98d087c22aa28c54f6f4357cd5b
Evgenii Ptitsyn <eptitsyn@gmail.com>
2021-12-18 13:35:26 +0300

    Lesson 2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String name 'John';
+    char sex = 'M';
+    float height = 170;
+    float weight = 62;
+    int age = 25;
+
+    public void walk() {
+        System.out.println(name+" is walking.");
+    }
+
+    public void sit() {
+        System.out.println(name+" is sitting.");
+    }
+
+    public void run() {
+        System.out.println(name+" is running.");
+    }
+
+    public void speak() {
+        System.out.println("Hi, Im " + name);
+    }
+
+    public void learnJava() {
+        System.out.println(name+" is walking.");
+    }
+}
